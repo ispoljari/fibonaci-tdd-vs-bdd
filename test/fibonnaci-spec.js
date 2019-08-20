fe792e9fc4249f889fb00f9666079829492da8b3
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-08-20 14:24:25 +0200

    Write unit test descriptions in comment

--- test/fibonnaci-spec.js
+++ test/fibonnaci-spec.js
@@ -1,1 +1,13 @@
 const assert = require('chai').assert;
+
+// define unit tests
+
+// should pass
+
+// 1. equals error if value of argument not integer
+// 2. equals error if value of arg int < 0
+// 3. equals [] if value of arg 0
+// 4. equals [1] if value of arg 1
+// 5. equals [1,1] if value of arg 2
+// 6. equals [1,1,2] if value of arg 3
+// 7. equals [1,1,2,3,5,8,13,21,34] if value of arg 9
