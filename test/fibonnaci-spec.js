81cd22b0495f968ee7dad3a2de75db1d09a5148f
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-08-20 14:39:49 +0200

    Define all unit tests in suite/test/assert format

--- test/fibonnaci-spec.js
+++ test/fibonnaci-spec.js
@@ -1,13 +1,38 @@
 const assert = require('chai').assert;
+const fib = require('../index');
 
-// define unit tests
+suite('fibonnaci', function() {
+  test('throws error if value of argument not integer', function() {
+    assert.throws(fib('Hey, I\'m a string'));
+    assert.throws(fib(true));
+    assert.throws(fib(null));
+  });
 
-// should pass
+  test('throws error if value of arg int < 0', function() {
+    assert.throws(fib(-1));
+  });
 
-// 1. equals error if value of argument not integer
-// 2. equals error if value of arg int < 0
-// 3. equals [] if value of arg 0
-// 4. equals [1] if value of arg 1
-// 5. equals [1,1] if value of arg 2
-// 6. equals [1,1,2] if value of arg 3
-// 7. equals [1,1,2,3,5,8,13,21,34] if value of arg 9
+  test('is array when int >= 0', function() {
+    assert.isArray(fib(1));
+  });
+
+  test('equals [] if value of arg 0', function() {
+    assert.equal([], fib(0));
+  });
+
+  test('equals [1] if value of arg 1', function() {
+    assert.equal([1], fib(1));
+  });
+
+  test('equals [1,1] if value of arg 2', function() {
+    assert.equal([1,1], fib(2));
+  });
+
+  test('equals [1,1,2] if value of arg 3', function() {
+    assert.equal([1,1,2], fib(3));
+  });
+
+  test('equals [1,1,2,3,5,8,13,21,34] if value of arg 9', function() {
+    assert.equal([1,1,2,3,5,8,13,21,34], fib(9));
+  });
+});
