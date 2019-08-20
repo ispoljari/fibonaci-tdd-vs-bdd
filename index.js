d93a78777c5d8582d219e2f956f057a3ecfc61ca
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-08-20 19:27:54 +0200

    Finnish the fib algorithm so that all the tests are passing

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ const fib = num => {
     throw new Error('The argument must be greater than or equal to 0!');
   }
 
-  return [];
+  if (num === 0) {
+    return [];
+  }
+
+  if (num === 1) {
+    return [1];
+  }
+
+  const arr = [1,1];
+
+  for(let i=2; i<num; i++) {
+    arr.push(arr[i-1] + arr[i-2]);
+  }
+
+  return arr;
 };
 
 module.exports = fib;
